43c879306f82628ffe75a3c7aa2a1224fe34cac9
amirvesal <amirvesal.se@outlook.com>
2024-06-30 23:44:14 +0330

    just fake payment done date count

--- firstProject/wwwroot/js/order.js
+++ firstProject/wwwroot/js/order.js
@@ -1,28 +1,52 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
-            { data: 'firstName', "width": "5%" },
-            { data: 'lastName', "width": "15%" },
-            { data: 'phoneNumber', "width": "20%" },
+            { data: 'firstName', "width": "10%" },
+            { data: 'lastName', "width": "10%" },
+            { data: 'phoneNumber', "width": "10%" },
             { data: 'applicationUser.email', "width": "15%" },
             { data: 'orderStatus', "width": "10%" },
-            { data: 'orderTotal', "width": "10%" },
-            { data: 'id',
+            { data: 'orderTotal', "width": "15%" },
+
+            {
+                data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2">ویرایش <i class="bi bi-pencil-square"></i></a>               
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
                     
                     </div>`
                 },
-                "width": "25%"
+                "width": "1%"
             }
         ]
     });
