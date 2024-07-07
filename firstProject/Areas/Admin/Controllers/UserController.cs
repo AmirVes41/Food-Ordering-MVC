db159753e9db9141919a99a72dc2b4cee98236d1
AmirVes41 <amirvesal.se@outlook.com>
2024-07-07 14:16:13 +0330

    user manageer and so on added
    just count if not working

---
+++ firstProject/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using firstProj.DataAccess.Repository.IRepository;
+using firstProj.DataAccess;
+using firstProj.Models;
+using firstProj.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Data;
+using firstProject.Areas.Admin.Controllers;
+using firstProj.Utility;
+using Microsoft.AspNetCore.Authorization;
+using firstProject.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstProject.Areas.Admin.Controllers
+{
+
+
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+           
+
+            return View();
+        }
+
+       
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in objUserList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = "-"
+                    };
+                }
+
+            }
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUn([FromBody]string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "خطا حین عملیات" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "عملیات موفقیت آمیز بود." });
+        }
+
+        #endregion
+    }
+}
---
+++ firstProject/wwwroot/js/user.js
@@ -0,0 +1,107 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: 'firstName', "width": "15%" },
+            { data: 'lastName', "width": "15%" },
+            { data: 'code', "width": "15%" },
+            { data: 'company.name', "width": "5%" },
+            { data: 'role', "width": "15%" },
+            {
+                data: { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+                             <a onclick=LockUn('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:auto;">
+                                    <i class="bi bi-lock-fill"></i> </a> 
+
+                        </div>
+                    `
+                    }
+                    else {
+                        return `
+                        <div class="text-center">
+                              <a onclick=LockUn('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:auto;">
+                                    <i class="bi bi-unlock-fill"></i></a>
+                        </div>
+                    `
+                    }
+                },
+                "width": "25%"
+            }
+        ]
+       
+    });
+}
+
+
+//delete function
+//function Delete(url) {
+//    const swalWithBootstrapButtons = Swal.mixin({
+//        customClass: {
+//            cancelButton: "btn btn-danger",
+//            confirmButton: "btn btn-success"
+//        },
+//        buttonsStyling: false
+//    });
+//    swalWithBootstrapButtons.fire({
+//        title: "آیا مطمنی؟",
+//        text: "این کار برگشت پذیر نیست",
+//        icon: "warning",
+//        showCancelButton: true,
+//        confirmButtonText: "آره،مطمنم",
+//        cancelButtonText: "نه،شک دارم",
+//        reverseButtons: true
+//    }).then((result) => {
+//        if (result.isConfirmed) {
+//            $.ajax({
+//                url: url,
+//                type: 'DELETE',
+//                success: function (data) {
+//                    dataTable.ajax.reload();
+//                    toastr.success(data.message);
+//                }
+//            })
+//            swalWithBootstrapButtons.fire({
+//                title: "حذف  شد",
+//                text: "فایل با موفیت حذف شد",
+//                icon: "success"
+//            });
+//        } else if (
+//            /* Read more about handling dismissals below */
+//            result.dismiss === Swal.DismissReason.cancel
+//        ) {
+//            swalWithBootstrapButtons.fire({
+//                title: "نگران نباش",
+//                text: "فعلا هیچی پاک نشده",
+//                confirmButtonText: "باشه",
+//                icon: "error"
+//            });
+//        }
+//    });
+//}
+
+function LockUn(id) {
+    $.ajax({
+        type: "POST",
+        url: '/Admin/User/LockUn',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+        }
+    });
+}
